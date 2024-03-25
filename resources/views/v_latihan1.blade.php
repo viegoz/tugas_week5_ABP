3bf6723d634c32ba74e95bf9b91797c78f55de9b
viego naufal <sabilwow@gmail.com>
2024-03-25 16:36:03 +0700

    update

--- resources/views/v_latihan1.blade.php
+++ resources/views/v_latihan1.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+
+    <link rel="stylesheet" href="{{url('css/home.css')}}">
 </head>
 <body>
     Nama : {{$nama}} <br/>
